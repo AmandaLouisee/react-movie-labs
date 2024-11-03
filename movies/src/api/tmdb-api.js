c72da68ecf1b9c8a06854e9af9b05b24d4932cd1
Amanda Brenden <amandalb@uia.no>
2024-11-03 18:44:56 +0000

    Added Upcoming movies page.

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -61,6 +61,25 @@ export const getMovie = (args) => {
       .then(json => json.production_countries);
   };
 
+
+
+  export const getUpcomingMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+        throw error
+    });
+  };
+
+
   
   
   export const getMovieImages = ({ queryKey }) => {
@@ -98,3 +117,4 @@ export const getMovie = (args) => {
       throw error
    });
   };
+
--- movies/src/index.js
+++ movies/src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route, Navigate, Routes} from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import UpcomingPage from "./pages/upcomingPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
-import AddMovieReviewPage from './pages/addMovieReviewPage'
+import AddMovieReviewPage from './pages/addMovieReviewPage';
+
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -30,6 +32,7 @@ const App = () => {
     <Routes>
       <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
       <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+      <Route path="/movies/upcoming" element={<UpcomingPage />} />
       <Route path="/movies/:id" element={<MoviePage />} />
       <Route path="/" element={<HomePage />} />
       <Route path="*" element={ <Navigate to="/" /> } />
